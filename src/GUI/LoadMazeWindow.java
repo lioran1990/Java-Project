735211ee6fe529e7d97cb88a62ef2e8de538f0af
lior.Ran <lior.ran1990@gmail.com>
2016-06-18 18:59:01 +0300

    lior is about to work on load maze

--- src/GUI/LoadMazeWindow.java
+++ src/GUI/LoadMazeWindow.java
@@ -3,14 +3,17 @@ package GUI;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
 public class LoadMazeWindow {
 
 	Shell loadMazeShell;
+	Display loadMazeDisplay;
 	Combo mazeList;
-	public LoadMazeWindow(Shell shell ,String list) {
+	public LoadMazeWindow(Shell shell , Display display,String list) {
 		loadMazeShell = new Shell (shell,SWT.TITLE | SWT.SYSTEM_MODAL | SWT.CLOSE | SWT.MAX);
+		loadMazeDisplay = display;
 		loadMazeShell.setLayout(new GridLayout(1, false));
 		loadMazeShell.setSize(500, 200);
 		loadMazeShell.setText("Load Maze");
